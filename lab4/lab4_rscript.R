241a834b46d58dd674b5874f3aaec72702da19a3
Jazz Zhang <z7zhang@ucsd.edu>
2023-11-22 13:59:32 -0800

    Add files from local computer

---
+++ lab4/lab4_rscript.R
@@ -0,0 +1,54 @@
+5+3
+5-3
+5*3
+5/3
+x <- 3*4
+x
+this_is_a_really_long_name <- 2.5
+r_rocks <- 2 ^ 3  
+
+seq(1,10)
+seq(1,10, by=2)
+example(seq)
+date()
+
+help(log)
+help.search("cross tabulate")
+
+example(log)
+
+length(3.1)
+x<-c(56, 95.3, 0.4)
+x
+y<-c(3.2, 1.1, 0.2)
+y
+x+y
+x/y
+sqrt(x)
+round(sqrt(x),3)
+log(x)/2+1
+
+x<-c(56, 95.3, 0.4)
+x[2]
+x[1]
+x[4]
+x[3]<-0.5
+x
+
+z <- c(TRUE, FALSE, FALSE, TRUE)
+z
+
+x <- 1:5
+x>3
+x >=3
+
+x<-1:5
+x[c(T,F,T,F,T)]
+
+x <- 1:50
+x
+plot(x)
+plot(x, sin(x))
+plot(x, sin(x), type = "l")
+plot(x, sin(x), type = "l", col = "blue")
+plot(x, sin(x), type = "l", col = "blue", lwd=3)
